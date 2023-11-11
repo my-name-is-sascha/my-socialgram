72e6cbc458ef6253558d4aeedb4c749acc2bdf97
sascha <sascha.mauch@endava.com>
2023-11-11 16:02:00 +0100

    created edit post functionality

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -10,16 +10,19 @@ import { Textarea } from "../ui/textarea"
 import FileUploader from "../shared/FileUploader"
 import { PostValidation } from "@/lib/validation"
 import { Models } from "appwrite"
-import { useCreatePost } from "@/lib/react-query/queriesAndMutations"
+import { useCreatePost, useDeletePost, useUpdatePost } from "@/lib/react-query/queriesAndMutations"
 import { useUserContext } from "@/context/AuthContext"
 import { useToast } from "../ui/use-toast"
  
 type PostFormProps = {
     post?: Models.Document,
+    action: "Create" | "Update",
 }
 
-const PostForm = ({ post }: PostFormProps) => {
+const PostForm = ({ post, action }: PostFormProps) => {
     const { mutateAsync: createPost, isPending: isLoadingCreate } = useCreatePost();
+    const { mutateAsync: updatePost, isPending: isLoadingUpdate } = useUpdatePost();
+
     const { user } = useUserContext();
     const { toast } = useToast();
     const navigate = useNavigate();
@@ -35,18 +38,36 @@ const PostForm = ({ post }: PostFormProps) => {
     })
     
     async function onSubmit(values: z.infer<typeof PostValidation>) {
-        const newPost = await createPost({
-            ... values, 
-            userId: user.id
+
+      if(post && action === 'Update') {
+        const updatedPost = await updatePost({
+          ...values,
+          postId: post.$id,
+          imageId: post?.imageId,
+          imageUrl: post.imageUrl
         })
 
-        if(!newPost) {
-            toast({
-                title: "Please try again."
-            })
-        }
+        if(!updatedPost) {
+          toast({
+            title: 'updating post failed. please try again'
+          })
 
-        navigate('/');
+          return navigate(`/posts/${post.$id}`)
+        }
+      } 
+
+      const newPost = await createPost({
+          ... values, 
+          userId: user.id
+      })
+
+      if(!newPost) {
+          toast({
+              title: "Please try again."
+          })
+      }
+
+      navigate('/');
     }
 
   return (
@@ -79,7 +100,10 @@ const PostForm = ({ post }: PostFormProps) => {
             <FormItem>
               <FormLabel className="shad-form_label">Add Photos</FormLabel>
               <FormControl>
-                <FileUploader fieldChange={field.onChange} mediaUrl={post?.imageUrl} />
+                <FileUploader 
+                  fieldChange={field.onChange} 
+                  mediaUrl={post?.imageUrl} 
+                />
               </FormControl>
               <FormMessage className="shad-form_message" />
             </FormItem>
@@ -134,8 +158,11 @@ const PostForm = ({ post }: PostFormProps) => {
             <Button 
                 type="submit" 
                 className="shad-button_primary whitespace-nowrap"
+                disabled={isLoadingCreate ||isLoadingUpdate}
+
             >
-                Submit
+              {isLoadingCreate || isLoadingUpdate && 'Loading ...'}
+              {action} Post
             </Button>
         </div>
       </form>
